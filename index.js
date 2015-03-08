d948c62f4bf7d6d186290cdfaa07e2c9176a0e28
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2015-03-08 22:22:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,49 @@
-var redis=require('redis');
-var client=redis.createClient(6379,'192.168.2.165');
+var redis=require("redis");
+var client=redis.createClient(6379,'127.0.0.1');
 client.on('error',function(err){
-	console.log('Error '+err);
+	console.log('ERROR'+err);
 });
 
 client.set('color','red',redis.print);
 client.get('color',function(err,value){
 	if(err) throw err;
-	console.log('color='+value);
+	console.log('Got:'+value);
 });
 
 client.hmset('camping',{
-	'shelter':'2-men-tent',
+	'shelter':'2-person tent',
 	'cooking':'campstove'
 },redis.print);
+
 client.hget('camping','cooking',function(err,value){
 	if(err) throw err;
-	console.log('camping='+value);
+	console.log('will cooking with :'+value);
 });
 
 client.hkeys('camping',function(err,keys){
 	if(err) throw err;
-	keys.forEach(function(key,i){
+	keys .forEach(function(key,i){
 		console.log(' '+key);
 	});
 });
 
-// var net=require('net');
-// var redis=require('redis');
-// var server=net.createServer(function(socket){
-// var subscriber;
-// var publisher;
-// socket.on('connect',function(){
-// 	subscriber=redis.createClient();
-// 	subscriber.subscribe('main_chat_room');
-// 	subscriber.on('message',function(channel,message){
-// 		socket.write('Channel '+channel+':'+message);
-// 	});
-// 	publisher=redis.createClient();
-// });
-// socket.on('data',function(data){
-// 	publisher.publish('main_chat_room',data);
 
-// });
-// socket.on('end',function(){
-// 	subscriber.unsubscribe('main_chat_room');
-// 	subscribe.end();
-// 	publisher.end();
-// });
+client.lpush('tasks','Paint the bike red',redis.print);
+client.lpush('tasks','Paint the bike green',redis.print);
+client.lrange('tasks',0,-1,function(err,items){
+	if(err) throw err;
+		items.forEach(function(item,i){
+			console.log(" "+item);
+		});
+});
 
-// });
 
-// server.listen(3000);
+client.sadd('ip_addresses','204.10.37.96',redis.print);
+client.sadd('ip_addresses','204.10.37.95',redis.print);
+client.sadd('ip_addresses','72,32,231,8',redis.print);
+
+client.smembers('ip_addresses',function(err,members){
+	if(err) throw err;
+	console.log(members);
+});
 
